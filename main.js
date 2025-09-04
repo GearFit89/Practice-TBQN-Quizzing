ca118522e94607880d0c1ce57e393ae6d9074651
GearFit89 <Jscram1775@gmail.com>
2025-09-04 17:07:24 -0400

    fixed undefined

--- main.js
+++ main.js
@@ -202,48 +202,42 @@ const quizApp = (function() {
     let selVerses =[];
      
 
-     function processQuestions(data) {
-       const  dataSplit = data.trim().split('\n');
-       let questiondict = {};
+    function processQuestions(data) {
+    const dataSplit = data.trim().split('\n');
+    let questiondict = {};
 
-
-
-        const regex = /^(\w+ \d+:\d+\w*\-?\d*) (\w) (?:(SQ|According to (?:\w+ \d+:\d+\w*\-?\d*))) (.*)$/;
-        
-        for (let val of dataSplit){
-            val = val.match(regex);
-            c+= 1;
-            if(val){
-
+    const regex = /^(\w+ \d+:\d+\w*\-?\d*) (\w) (?:(SQ|According to (?:\w+ \d+:\d+\w*\-?\d*))) (.*)$/;
+    
+    for (let val of dataSplit){
+        const match = val.match(regex);
+        c+= 1;
+        if(match){ // check if a match was found
             const monthIndex = Math.floor((c - 1) / 20);
             const monthName = quiMonths[monthIndex];
-            const ref1 = val[1];
-            const flight1 = val[2];
+            const ref1 = match[1];
+            const flight1 = match[2];
             
-            const typeQuestion = val[3];
+            const typeQuestion = match[3];
+            let type1; // declare type1 with a broader scope
             if (typeof (typeQuestion) != 'string' ){
-                const type1 = 'question'
+                type1 = 'question';
             }else{
-                const type1 = typeQuestion
+                type1 = typeQuestion;
             };
-            const questAns = val[4] // maybe use the regex to split between quest and ans
+            const questAns = match[4];
 
-            }
             questiondict[c] = {
                 flight: flight1,
                 verse: questAns,
                 ref: ref1,
                 month: monthName,
                 type: type1
-                
             };
-
-
-        };
-        console.log('processed quotes', questiondict)
-        return questiondict
-
-     }
+        }
+    }
+    console.log('processed quotes', questiondict);
+    return questiondict;
+ }
     function processQuotes(quotesFTVs) {
         c = 0
         const data = quotesFTVs.trim().split("\n");
